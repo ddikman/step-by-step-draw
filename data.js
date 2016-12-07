0cca85a100f97f659556ea1129f8e75410dd9f36
David Dikman <ddikman@gmail.com>
2016-12-07 17:25:46 +0000

    Now works with names stored in seperate file, displays initials

--- data.js
+++ data.js
@@ -1,3 +1,29 @@
-var data = [11111, 12345, 12345, 21323, 21321, 23123, 23123, 23132, 23123];
-data = data.sort();
-console.log('got ' + data.length + ' items to begin with');
+
+function processData(data, callback){
+  data = data.split('\n').filter(function(n) { return n.length > 0; })
+  console.log(data);
+  data = data.map(function(name, index){
+    name = name.trim();
+    initials = name.split(' ').reduce(function(final, curr){
+      return final + curr[0].toUpperCase()
+    }, '');
+    return {
+      masked: initials,
+      unmasked: name,
+      id: index
+    };
+  });
+
+  console.log('got ' + data.length + ' items.');
+  callback(data);
+}
+
+function loadData(callback) {
+  $.ajax({
+      url : "names.txt",
+      dataType: "text",
+      success : function (data) {
+          processData(data, callback)
+      }
+  });
+}
--- draw.js
+++ draw.js
@@ -1,85 +1,98 @@
-function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
+// Draw configuration
+drawStep = 4;
+draws = [1, 3, 5, 10];
+drawSpeed = [2500, 1500, 1000, 500];
+// drawSpeed = draws.map(function(i) { return 1; }); // minimum for all steps
 
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
+if (draws.length != drawSpeed.length)
+  throw 'Number of elements to leave each draw needs to match draw speed.';
 
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
+function remove(data) {
+  var removeIndex  = Math.floor(Math.random() * data.length);
+  item = data[removeIndex]
+  console.log('removing ' + item.id);
+  $('#item-' + item.id).addClass('hover');
+  data.splice(removeIndex, 1);
 
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
-}
-
-var index = 0;
-var root = Math.ceil(Math.sqrt(data.length));
-var width = ($(window).width() / root) - 20;
-var height = ($(window).height() / root) - 18;
-console.log('drawing ' + data.length + ' items');
-for(r = 0; r < root; ++r)
-{
-  row = $('<tr/>');
-  for(c = 0; c < root; ++c)
-  {
-    if(index >= data.length)
-      break;
-
-    console.log('drawing item ' + index);
-    attendeeNumber = data[index++];
-
-    flipContainer = $('<div/>');
-    flipContainer.addClass('flip-container');
-    flipContainer.attr('id', 'item-' + parseInt(attendeeNumber));
-    flipContainer.css({'width': width + 'px', 'height': height + 'px'})
-
-    flipper = $('<div/>');
-    flipper.addClass('flipper');
-    flipContainer.append(flipper);
-
-    front = $('<div><span class="text">' + attendeeNumber + '</span></div>');
-    front.addClass('front');
-    front.css({'width': width + 'px', 'height': height + 'px'})
-    flipper.append(front);
-
-    back = $('<div><span class="text">' + attendeeNumber + '</span></div>');
-    back.addClass('back');
-    back.css({'width': width + 'px', 'height': height + 'px'})
-    flipper.append(back);
-
-    var column = $('<td/>');
-    column.append(flipContainer);
-    row.append(column);
-  }
-  $('#table').append(row);
-}
-
-data = shuffle(data);
-console.log('reshuffle stuff')
-
-drawStep = 7;
-draws = [1, 2, 3, 5, 10, 20, 30];
-drawSpeed = [1000, 2500, 2500, 1500, 300, 200, 10];
-//drawSpeed = [1, 1,1, 1,1,1,1,1,1,1]
-
-function remove() {
-  var item = Math.floor(Math.random()*data.length);
-  console.log('removing ' + data[item] + '>' + item + ' of ' + data.length + ' items.');
-  $('#item-' + data[item]).addClass('hover');
-  data.splice(item, 1);
-
-  if(data.length > draws[drawStep])
-    setTimeout(function(){ remove() }, drawSpeed[drawStep]);
+  console.log('drawsstep = ' + drawStep)
   if(data.length == 1)
-    $('#item-' + data[0] + " .front").animate({'background-color': '#a8a8ff', 'border-color': '#a8a8ff'}, 1500);
+  {
+    last_item = data[0];
+    $('#item-' + last_item.id + " .front").animate({'background-color': '#a8a8ff', 'border-color': '#a8a8ff'}, 1500);
+    $('#item-' + last_item.id + ' span.text').text(last_item.unmasked);
+  }
+  else if(data.length > draws[drawStep])
+  {
+    console.log('running remove ' + (data.length - draws[drawStep]) + ' from ' + data.length + ' items in ' + drawSpeed[drawStep] + 's');
+    setTimeout(function(){ remove(data) }, drawSpeed[drawStep]);
+  }
 };
 
-$(document).keydown(function(){
-  drawStep--;
-  setTimeout(function(){ remove()}, drawSpeed[drawStep]);
+function initialize(data){
+  data = shuffle(data);
+  console.log('shuffled data')
+
+  // Calculate box size
+  var root = Math.ceil(Math.sqrt(data.length));
+  var width = ($(window).width() / root) - 20;
+  var height = ($(window).height() / root) - 18;
+
+  console.log('drawing ' + data.length + ' items');
+  var index = 0;
+  for(r = 0; r < root; ++r)
+  {
+    row = $('<tr/>');
+    for(c = 0; c < root; ++c)
+    {
+      if(index >= data.length)
+        break;
+
+      item = data[index++]
+      console.log('drawing ' + item.id + ', ' + item.masked + ', ' + item.unmasked)
+
+      flipContainer = $('<div/>');
+      flipContainer.addClass('flip-container');
+      flipContainer.attr('id', 'item-' + item.id);
+      flipContainer.css({'width': width + 'px', 'height': height + 'px'})
+      flipContainer.attr('title', item.unmasked);
+
+      flipper = $('<div/>');
+      flipper.addClass('flipper');
+      flipContainer.append(flipper);
+
+      front = $('<div><span class="text">' + item.masked + '</span></div>');
+      front.addClass('front');
+      front.css({'width': width + 'px', 'height': height + 'px'})
+      flipper.append(front);
+
+      back = $('<div><span class="text">' + item.masked + '</span></div>');
+      back.addClass('back');
+      back.css({'width': width + 'px', 'height': height + 'px'})
+      flipper.append(back);
+
+      var column = $('<td/>');
+      column.append(flipContainer);
+      row.append(column);
+    }
+    $('#table').append(row);
+  }
+
+  $(document).keydown(function(){
+    drawStep--;
+    console.log('running remove ' + (data.length - draws[drawStep]) + ' from ' + data.length + ' items in ' + drawSpeed[drawStep] + 's');
+    setTimeout(function(){ remove(data)}, drawSpeed[drawStep]);
+  });
+}
+
+loadData(function(data){
+  firstDraw = draws[drawStep - 1]
+  if (data.length < firstDraw)
+  {
+    var error = $('<span class="error"/>')
+    error.text('First draw is configured to leave ' + firstDraw + ' elements but \
+  only ' + data.length + ' items were supplied.')
+    $('body').append(error)
+  }
+  else
+    initialize(data);
 });
